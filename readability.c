6ff63c4612212953fcc9e2329aa76a01bd8b7fa7
fiamaviana <fiamaviana@users.noreply.github.com>
2020-05-23 14:14:00 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -23,7 +23,7 @@ int main (void)
     float L = (num_letters / (float) num_words) * 100;
     float S = (num_sentences / (float) num_words) * 100;
     int index = round(0.0588 * L - 0.296 * S - 15.8);
-    if (index < 1)
+    if (index <= 1)
     printf("Before Grade 1\n ");
     else if (index >= 16)
     printf ("Grade 16+\n");
