6dd08e4819cfd7fc96aafbbb03a3b0bea91c6529
fiamaviana <fiamaviana@users.noreply.github.com>
2020-07-14 15:22:09 +0000

    automated commit by check50 [check50=True]

--- readability.py
+++ readability.py
@@ -15,8 +15,8 @@ L = letters / words * 100
 S = sentences / words * 100
 index = round(0.0588 * L - 0.296 * S - 15.8)
 if index < 1:
-    print("Before Grade 1\n")
+    print("Before Grade 1")
 elif (index >= 16):
-    print("Grade 16+\n")
+    print("Grade 16+")
 else:
-    print(f"Grade{index}\n")
+    print(f"Grade{index}")
