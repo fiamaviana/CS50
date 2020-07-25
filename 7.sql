e01f6bf6ccd89d77f92500eae66bda5bf6c757bf
fiamaviana <fiamaviana@users.noreply.github.com>
2020-07-25 18:07:22 +0000

    automated commit by check50 [check50=True]

--- 7.sql
+++ 7.sql
@@ -1,4 +1,4 @@
 SELECT movies.title, ratings.rating
 FROM ratings, movies
 WHERE movies.id = ratings.movie_id
-AND movies.year = '2010'ORDER BY ratings.rating DESC
+AND movies.year = '2010'ORDER BY ratings.rating DESC;
